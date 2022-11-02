fbb38156bb44a58a21e5479d365dc62a82a6e3da
igorl <g00390368@atu.ie>
2022-11-02 19:54:50 +0000

    added junit and test

---
+++ src/test/java/ie/atu/PassengerTest.java
@@ -0,0 +1,26 @@
+package ie.atu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassengerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+}
